a5913e41d70496ccdf70cba11bb6c026e8256477
DanielleColucci <ddcolucci99@gmail.com>
2023-01-27 13:38:23 -0500

    create a Header component with a link to the root route rendered by App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 import StarshipList from './pages/StarshipList/StarshipList'
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path='/' element={<StarshipList />} /> 
       </Routes>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <Link to='/'>STAR WARS STARSHIPS</Link>
+  )
+}
+export default Header

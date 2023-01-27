b003a401be858e68954b86767de918fde4288cb9
DanielleColucci <ddcolucci99@gmail.com>
2023-01-27 13:50:50 -0500

    write service function to fetch all starships; hold starships in state and use service function to fetch all as an effect

--- src/pages/StarshipList/StarshipList.jsx
+++ src/pages/StarshipList/StarshipList.jsx
@@ -1,4 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { getAllStarships } from '../../services/sw-api';
+
 const StarshipList = () => {
+  const [starships, setStarships] = useState([])
+
+  useEffect(() => {
+    const fetchStarshipList = async () => {
+      const starshipData = await getAllStarships()
+      setStarships(starshipData)
+    }
+    fetchStarshipList()
+  }, [])
+
   return (
     <div>List of ships goes here</div>
   )
